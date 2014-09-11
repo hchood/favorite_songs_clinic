46eedc20ccab51a77ac84ccc64b0b01d4f328fe4
Helen Hood <helen.c.hood@gmail.com>
2014-09-11 16:07:11 -0400

    Add associations with FavoriteSong

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,7 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+  has_many :favorite_songs
+  has_many :users, through: :favorite_songs
 
   validates :name,
     presence: true,
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  has_many :favorite_songs
+  has_many :songs, through: :favorite_songs
+
   validates :name, presence: true
   validates :email,
     presence: true,
