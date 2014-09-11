bdeaf475e4061a3480140f349cf0dbab393542a4
Helen Hood <helen.c.hood@gmail.com>
2014-09-11 15:52:30 -0400

    Add artist_id to songs table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140911194639) do
+ActiveRecord::Schema.define(version: 20140911195102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20140911194639) do
     t.string   "name",       null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "artist_id"
   end
 
   create_table "users", force: true do |t|
